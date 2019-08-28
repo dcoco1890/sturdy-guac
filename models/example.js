dc6933a9dafad5dcfcf6df06c6aa55ccb13578a0
Red Tail <redtail814@gmail.com>
2019-08-28 14:52:43 -0400

    example.js lint

--- models/example.js
+++ models/example.js
@@ -1,11 +1,11 @@
 module.exports = function(sequelize, DataTypes) {
-    var Example = sequelize.define("Example", {
-        text: DataTypes.STRING,
-        description: DataTypes.TEXT
-    }, {
-        timestamps: false,
-    });
-    return Example;
+  var Example = sequelize.define("Example", {
+    text: DataTypes.STRING,
+    description: DataTypes.TEXT
+  }, {
+    timestamps: false,
+  });
+  return Example;
 };
 
 // timestamps false removes the createdat and updated at
