9960f19a11256ec805731e22bfb59ca1468482ee
Red Tail <redtail814@gmail.com>
2019-08-28 14:45:03 -0400

    linted config.js

--- config/config.js
+++ config/config.js
@@ -1,24 +1,24 @@
 module.exports = {
-    "development": {
-        "username": process.env.DB_USER,
-        "password": process.env.DB_PASS,
-        "database": process.env.DB_NAME,
-        "host": process.env.DB_HOST,
-        "dialect": "mysql"
-    },
-    "test": {
-        "username": "root",
-        "password": null,
-        "database": "testdb",
-        "host": "localhost",
-        "dialect": "mysql",
-        "logging": false
-    },
-    "production": {
-        "use_env_variable": "JAWSDB_URL",
-        "dialect": "mysql"
-    }
-}
+  "development": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "testdb",
+    "host": "localhost",
+    "dialect": "mysql",
+    "logging": false
+  },
+  "production": {
+    "use_env_variable": "JAWSDB_URL",
+    "dialect": "mysql"
+  }
+};
 
 //changed this file to .js, and added module.exports at the top.
 // this is in order to use the process.env variables
