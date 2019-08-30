e95add093338ba9abe652ce1f30a9995f7df2844
Cohen <dcoco91@gmail.com>
2019-08-29 21:22:13 -0400

    added a route to add images

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,34 +2,38 @@ const db = require("../models");
 
 module.exports = app => {
 
-  // index page
-  app.get("/", (req, res) => {
-    db.Example.findAll({}).then(dbExamples => {
-      res.render("index", {
-        msg: "SKREET_art",
-        examples: dbExamples
-      });
+    // index page
+    app.get("/", (req, res) => {
+        db.Example.findAll({}).then(dbExamples => {
+            res.render("index", {
+                msg: "SKREET_art",
+                examples: dbExamples
+            });
+        });
     });
-  });
 
-  // map page
-  app.get("/map", (req, res) => {
-    db.Example.findAll({}).then(() => {
-      res.render("map", {});
+    // map page
+    app.get("/map", (req, res) => {
+        db.Example.findAll({}).then(() => {
+            res.render("map", {});
+        });
     });
-  });
 
-  // example page and pass in an example by id
-  app.get("/example/:id", (req, res) => {
-    db.Example.findOne({ where: { id: req.params.id } }).then(dbExample => {
-      res.render("example", {
-        example: dbExample
-      });
+    // example page and pass in an example by id
+    app.get("/example/:id", (req, res) => {
+        db.Example.findOne({ where: { id: req.params.id } }).then(dbExample => {
+            res.render("example", {
+                example: dbExample
+            });
+        });
     });
-  });
 
-  // 404 page for any unmatched routes
-  app.get("*", (req, res) => {
-    res.render("404");
-  });
+    app.get("/upload", (req, res) => {
+        res.render("upload");
+    });
+
+    // 404 page for any unmatched routes
+    app.get("*", (req, res) => {
+        res.render("404");
+    });
 };
