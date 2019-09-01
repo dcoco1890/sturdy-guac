984505f6f1e1f0cf9db8b5ff8315b1af01db5458
Red Tail <redtail814@gmail.com>
2019-09-01 19:43:36 -0400

    Deleted example reference code.

--- public/js/map.js
+++ public/js/map.js
@@ -1,7 +1,8 @@
 var map;
+
 function initMap() {
-  map = new google.maps.Map(document.getElementById('mapBox'), {
-    center: {lat: 37.5407, lng: -77.4360},
-    zoom: 10
-  });
+    map = new google.maps.Map(document.getElementById("mapBox"), {
+        center: {lat: 37.5407, lng: -77.4360},
+        zoom: 10
+    });
 }
