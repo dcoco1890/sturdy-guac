7c9349c569c18c929c627f1f34a15425c78b9731
Red Tail <redtail814@gmail.com>
2019-09-06 14:39:19 -0400

    Update routes and html to fit into template

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,35 +3,25 @@ const db = require("../models");
 module.exports = app => {
 
     // home page
-    // I removed the DB call here, currently we are not pulling any images
-    // here so there was no need.
     app.get("/", (req, res) => {
         res.render("index");
-
+    });
+    
+    // upload page
+    app.get("/upload", (req, res) => {
+        res.render("map-upload");
     });
 
     // map page
     app.get("/map", (req, res) => {
-        res.render("map", {});
-    });
-
-    app.get("/map/all", (req, res) => {
-        res.render("all-markers");
-    });
-
-    // future project, render specific marker from DB
-    // app.get("/map/:id", (req, res) => {
-    //     db.Image.findOne({ where: { id: req.params.id } }).then(dbImage => {
-    //         var x = dbImage;
-    //         console.log(x); //thnis code is nothing
-    //         res.render("404");
-    //     });
-    // });
-
-    // upload page
-    // we might not need this page anymore
-    app.get("/upload", (req, res) => {
-        res.render("upload");
+        db.Image.findAndCountAll({}).then(dbImages => {
+            console.log(dbImages.count);
+            // console.log(dbImages.rows);
+            console.log(`\n${dbImages.count} pictures found! \n`);
+            res.render("map-gallery", {
+                images: dbImages.rows
+            });
+        });
     });
 
     // gallery page
@@ -41,7 +31,6 @@ module.exports = app => {
             // console.log(dbImages.rows);
             console.log(`\n${dbImages.count} pictures found! \n`);
             res.render("gallery-body", {
-                layout: "gallery",
                 images: dbImages.rows
             });
         });
@@ -60,4 +49,14 @@ module.exports = app => {
     app.get("*", (req, res) => {
         res.render("404");
     });
+    
+    // future project, render specific marker from DB
+    // app.get("/map/:id", (req, res) => {
+    //     db.Image.findOne({ where: { id: req.params.id } }).then(dbImage => {
+    //         var x = dbImage;
+    //         console.log(x); //thnis code is nothing
+    //         res.render("404");
+    //     });
+    // });
+
 };
