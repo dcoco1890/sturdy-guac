c1236f73c18fd7b3e48760bb1e32812a89b6a4c3
Jason Novak <novak.jason7@gmail.com>
2019-09-07 01:18:38 -0400

    Removed controllers

--- controllers/views-controller.js
+++
@@ -1,8 +0,0 @@
-const express  = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => res.render('home', { user: req.user }));
-
-router.get('/register', (req, res) => res.render('home', { user: req.user }));
-
-module.exports = router;
